b86dab83f65dee33bd485fca6f5adf445fec2df9
NKNY <norman.knyazev@gmail.com>
2018-04-29 09:30:37 +0200

    Fixed POST on profile page

--- app.py
+++ app.py
@@ -8,25 +8,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-<<<<<<< HEAD
     return render_template('profile_page.html')
 
 
-@app.route('/receiver', methods = ['POST'])
-def worker():
+@app.route('/no', methods = ['post'])
+def handle_data():
     print("POST request")
-    print(request.is_json)
-    content = request.get_json()
-    print(content['device'])
-=======
-    return render_template('navbar.html')
->>>>>>> 86649cf8478a067dd62b9278dbcc7d22737378fd
-
-#	data = request.get_json()
-#    result = ''
-
-    #for item in data:
-    #    # loop over every row
-    #    result += str(item['device']) + '\n'
-
-    return 'JSON posted'
+    print(request.form)
+#     print(request.is_json)
+#     content = request.get_json()
+#     print(content['device'])
+#     return render_template('navbar.html')
