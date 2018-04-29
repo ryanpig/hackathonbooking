931ba427d888f828fcca5846294cde0f262d2cda
ryanpig <ryanpig@gmail.com>
2018-04-29 08:49:10 +0200

    profile_page_2

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 
-from flask import render_template
+from flask import render_template, request, redirect, Response
 from flask import Flask
+import random, json
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('profile_page.html')
 
 
+@app.route('/receiver', methods = ['POST'])
+def worker():
+    print("POST request")
+    print(request.is_json)
+    content = request.get_json()
+    print(content['device'])
 
+#	data = request.get_json()
+#    result = ''
+
+    #for item in data:
+    #    # loop over every row
+    #    result += str(item['device']) + '\n'
+
+    return 'JSON posted'
